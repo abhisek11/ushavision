df31fd0d58a160ad214ff525742b49808148b559
abhisek11 <abhisekrock94@gmail.com>
2020-01-01 22:46:04 +0530

    sharpening

---
+++ vision_projects/sharpening.py
@@ -0,0 +1,15 @@
+import cv2
+import numpy as np 
+
+image = cv2.imread('..\\MasterOpenCV\\images\\elephant.jpg')
+cv2.imshow("original image",image)
+cv2.waitKey(0)
+
+kernel_sharpening = np.array([[-1,-1,-1],
+                              [-1,9,-1],
+                              [-1,-1,-1]])
+
+sharpened = cv2.filter2D(image,-1,kernel_sharpening)
+cv2.imshow("image sharpening ",sharpened)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
